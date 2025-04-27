be3ab7ccb6047cc73bf8238ccd4b03c975ad273a
Rafael Feliz <rafafelizti@gmail.com>
2025-04-27 11:55:22 -0300

    Atualizar o Documentos.java

--- trabalho-estruturas-Rafael/estruturas/Documentos.java
+++ trabalho-estruturas-Rafael/estruturas/Documentos.java
@@ -1,1 +1,24 @@
-oi
+package estruturas;
+
+public class Documentos {
+    private String nome;
+    private int tamanho;
+
+    public Documentos(String nome, int tamanho) {
+        this.nome = nome;
+        this.tamanho = tamanho;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getTamanho() {
+        return tamanho;
+    }
+
+    @Override
+    public String toString() {
+        return "Documento: " + nome + " (" + tamanho + " KB)";
+    }
+}
